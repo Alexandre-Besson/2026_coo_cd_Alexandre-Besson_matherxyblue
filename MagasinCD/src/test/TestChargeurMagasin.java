57efa83b2b2cea02e4a3bedfcedf912c2dfcc665
Mathieu Allibert-Millot <e24719u@etu.univ-lorraine.fr>
2026-05-05 15:55:19 +0200

    test chargeur magasin

--- MagasinCD/src/test/TestChargeurMagasin.java
+++ MagasinCD/src/test/TestChargeurMagasin.java
@@ -1,4 +1,32 @@
 package test;
 
+import XML.ChargeurMagasin;
+import donnees.Magasin;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestChargeurMagasin {
+
+
+    @Test
+    public void test() throws FileNotFoundException {
+        ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../MagasinCD/musicbrainzSimple/");
+        Magasin magasin = chargeurMagasin.chargerMagasin();
+        assertEquals(true,magasin.getNombreCds()>0);
+    }
+
+    @Test
+    public void test1() {
+        boolean bool=false;
+        try{
+            ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../MagasinCD/musicbrainSimple/");
+            Magasin magasin = chargeurMagasin.chargerMagasin();
+        }catch(FileNotFoundException e){
+            bool=true;
+        }
+        assertEquals(true,bool);
+    }
 }
