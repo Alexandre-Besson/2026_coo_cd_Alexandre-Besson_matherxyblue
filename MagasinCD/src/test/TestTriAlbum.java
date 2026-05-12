f265ee9b811640aea2a3c05202693e2b73a14d1e
Mathieu Allibert-Millot <e24719u@etu.univ-lorraine.fr>
2026-05-12 15:20:26 +0200

    test

--- MagasinCD/src/test/TestTriAlbum.java
+++ MagasinCD/src/test/TestTriAlbum.java
@@ -1,6 +1,7 @@
 package test;
 
 import XML.ChargeurMagasin;
+import donnees.ComparateurAlbum;
 import donnees.Magasin;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ public class TestTriAlbum {
     public void test() throws FileNotFoundException {
         ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../MagasinCD/musicbrainzSimple/");
         Magasin magasin = chargeurMagasin.chargerMagasin();
-        magasin.trierAlbum();
+        magasin.trier(new ComparateurAlbum());
         assertEquals("Believe",magasin.getCd(0).getNomCD());
         assertEquals("Whitney Houston",magasin.getCd(11).getNomCD());
     }
--- MagasinCD/src/test/TestTriArtiste.java
+++ MagasinCD/src/test/TestTriArtiste.java
@@ -1,6 +1,7 @@
 package test;
 
 import XML.ChargeurMagasin;
+import donnees.ComprateurArtiste;
 import donnees.Magasin;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ public class TestTriArtiste {
     public void test() throws FileNotFoundException {
         ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../MagasinCD/musicbrainzSimple/");
         Magasin magasin = chargeurMagasin.chargerMagasin();
-        magasin.trierArtiste();
+        magasin.trier(new ComprateurArtiste());
         assertEquals("Bénabar",magasin.getCd(0).getNomArtiste());
         assertEquals("Zebda",magasin.getCd(11).getNomArtiste());
     }
